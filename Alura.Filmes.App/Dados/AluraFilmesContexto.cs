ab6a894fb6a41819e47a3ec6040f0ad2fef69024
= <=>
2022-01-07 08:22:32 -0300

    Commit Inicial

---
+++ Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,27 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> actor { get; set; }
+        public DbSet<Filme> film { get; set; }
+        public DbSet<FilmeAtor> Elenco { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Initial Catalog=AluraFilmes; Integrated Security=True");
+
+      
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfiguration(new AtorConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeAtorConfiguration());
+        }
+    }
+}
---
+++ Alura.Filmes.App/Program.cs
@@ -0,0 +1,53 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //select * from actor
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var filme = contexto.film
+                    .Include(f => f.Atores)   //inner join
+                    .ThenInclude(fa => fa.Ator)
+                    .First();
+
+                Console.WriteLine(filme);
+                Console.WriteLine("Elenco");
+
+                //listar os 10 atores modificados recentemente 
+                //var atores = contexto.actor
+                //    .OrderByDescending(a => EF.Property<DateTime>(a, "last_update"))
+                //    .Take(10);
+
+                //foreach (var ator in atores)
+                //{
+                //    Console.WriteLine(ator + " - " + contexto.Entry(ator).Property("last_update").CurrentValue);
+                //}
+
+                //listar todos os filmes
+                //foreach (var item in contexto.Elenco)
+                //{
+                //    var entidade = contexto.Entry(item);
+                //    var filmId = entidade.Property("film_id").CurrentValue;
+                //    var actorId = entidade.Property("actor_id").CurrentValue;
+                //    var lastUpd = entidade.Property("last_update").CurrentValue;
+                //    Console.WriteLine($"Filme: {filmId}, Ator: {actorId}, LastUpdate: {lastUpd}");
+                //}
+
+                foreach (var ator in filme.Atores)
+                {
+                    Console.WriteLine(ator.Ator);
+                }
+            }
+        }
+    }
+}
