a3755eae11f590a1d392de8a7f75dd271c8566d1
= <=>
2022-01-08 08:33:25 -0300

    Atualizações - 08010833

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Alura.Filmes.App.Dados;
 using Alura.Filmes.App.Extensions;
 using Alura.Filmes.App.Negocio;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 
@@ -15,6 +14,10 @@ namespace Alura.Filmes.App
             {
                 contexto.LogSQLToConsole();
 
+                //var m10 = ClassificacaoIndicativa.MaioresQue18;
+                //Console.WriteLine(m10.ParaString());
+                //Console.WriteLine("G".ParaValor());
+
                 //var ator1 = new Ator { PrimeiroNome = "Emma", UltimoNome = "Watson" };
                 //var ator2 = new Ator { PrimeiroNome = "Emma", UltimoNome = "Watson" };
                 //contexto.Atores.AddRange(ator1, ator2);
@@ -25,16 +28,19 @@ namespace Alura.Filmes.App
                 //Console.WriteLine($"Total de atores encontrados: {emmaWatson.Count()}.");
 
                 var filme = new Filme();
-                var idioma = new Idioma { Nome = "English" };
-                filme.Titulo = "Senhor dos Aneis";
+                var idioma = new Idioma { Nome = "English" }; //quando não existe na tabela Language
+                filme.Titulo = "Cassino Royale";
                 filme.Duracao = 120;
                 filme.AnoLancamento = "2000";
-                filme.Classificacao = "Ótimo";
-                filme.IdiomaFalado = idioma;
+                filme.Classificacao = ClassificacaoIndicativa.MaioresQue14;
+                filme.IdiomaFalado = contexto.Idiomas.First();
                 contexto.Entry(filme).Property("last_update").CurrentValue = DateTime.Now;
 
                 contexto.Filmes.Add(filme);
                 contexto.SaveChanges();
+
+                var filmeInserido = contexto.Filmes.First(f => f.Titulo == "Cassino Royale");
+                Console.WriteLine(filmeInserido.Classificacao);
 
             }
         }
